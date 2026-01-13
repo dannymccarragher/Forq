06781fb21a6acabec14402b8b0c98316b5e6feca
Danny McCarragher <dannymccarragher@gmail.com>
2026-01-12 22:54:13 -0800

    mount auth routes

--- apps/api/router/router.js
+++ apps/api/router/router.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 import foodController from "../controller/foodController.js";
 import foodDBController from "../controller/foodDBController.js";
 import userController from "../controller/userController.js";
+import authController from "../controller/authController.js";
 
 const router = Router();
+
+// Authentication routes
+router.use("/api/auth", authController);
 
 // FatSecret API routes
 router.use("/api", foodController);
